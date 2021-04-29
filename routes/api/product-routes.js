f4c18539521dc1722ae11440b2f659e5ef9389d1
Robert Adams <adamsiii.robert@gmail.com>
2021-04-28 19:56:34 -0700

    added get all products route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,14 +3,22 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+// get all products including associated category and tags
+router.get('/', async (req, res) => {
+
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag}],
+    });
+    res.status(200).json(productData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
@@ -89,7 +97,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
 });
 

3439d8611a475ecc023f935750695fe1852f844d
Robert Adams <adamsiii.robert@gmail.com>
2021-04-28 18:44:37 -0700

    added post route to create new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -26,7 +26,7 @@ router.get('/:id', async (req, res) => {
     });
 
     if (!categoryData) {
-      res.status(404).json({ message: 'No library card found with that id!' });
+      res.status(404).json({ message: 'No category found with that id!' });
       return;
     }
     res.status(200).json(categoryData);
@@ -36,15 +36,23 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// create a new category
+router.post('/', async (req, res) => {
+
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
 });
 

b23dc2a404aa64ea902c0378a74b0c787f0104a1
Robert Adams <adamsiii.robert@gmail.com>
2022-09-10 14:23:01 -0700

    Remove comments

--- models/Product.js
+++ models/Product.js
@@ -1,42 +1,38 @@
-// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
 const sequelize = require('../config/connection');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
 Product.init(
   {
-    id: {                        // id
-      type: DataTypes.INTEGER,   // integer
-      allowNull: false,          // doesn't allow null values
-      primaryKey: true,          // set as primary key
-      autoIncrement: true,       // uses auto increment
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    product_name: {              // product_name
-      type: DataTypes.STRING,    // string
-      allowNull: false,          // doesn't allow null values
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    price: {                     // price
-      type: DataTypes.DECIMAL,   // decimal
-      allowNull: false,          // doesn't allow null values
-      validate: {                // validates that the value is a decimal
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
         isDecimal: true,
       },
     },
-    stock: {                     // stock
-      type: DataTypes.INTEGER,   // integer
-      allowNull: false,          // doesn't allow null values
-      defaultValue:10,           // set a default value of 10.
-      validate: {                // validates that the value is numeric
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
         isNumeric: true,
       },
     },
-    category_id: {               // category_id
-      type: DataTypes.INTEGER,   // integer
-      references: {              // references the category model's id.
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
         model: 'category',
         key: 'id',
       },
